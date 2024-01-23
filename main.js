9396803689306b5185b06a07212b3051b2d314c4
Farias <51142277+fariasu@users.noreply.github.com>
2024-01-23 10:41:54 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,77 @@
+// // 1
+// alert("Isso é um alerta.");
+
+// //2
+// let numberOne = 1;
+// let numberTwo = 2;
+
+// alert(Number(numberOne) + Number(numberTwo));
+
+// //3
+// let optionOne = "1";
+// let optionTwo = 1;
+
+// if(typeof optionTwo == "number"){
+//     alert("É um número.");
+// }else{
+//     alert("Não é um número.");
+// }
+
+// //4
+// let optionOne = "1";
+// let optionTwo = 1;
+
+// if(typeof optionTwo == "string"){
+//     alert("É uma string.");
+// }else{
+//     alert("Não é uma string.");
+// }
+
+// //5
+// let optionOne = "1";
+// let optionTwo = true;
+
+// if(typeof optionTwo == "boolean"){
+//     alert("É uma booleana.");
+// }else{
+//     alert("Não é uma booleana.");
+// }
+
+// //6
+// let numberOne = 10;
+// let numberTwo = 5;
+
+// alert(numberOne - numberTwo);
+
+// //7
+// let numberOne = 10;
+// let numberTwo = 5;
+
+// alert(numberOne * numberTwo);
+
+//8 
+// let numberOne = 10;
+// let numberTwo = 5;
+
+// alert(numberOne / numberTwo);
+
+// //9
+// let isEven = 6;
+// let result = isEven % 2;
+
+// if(result == 0){
+//     alert("É um número par.");
+// }else{
+//     alert("Não é um número par.");
+// }
+
+// //10
+// let isOdd = 5;
+// let result = isOdd % 2;
+
+// if(result != 0){
+//     alert("É um número ímpar.");
+// }else{
+//     alert("Não é um número ímpar.");
+// }
+
